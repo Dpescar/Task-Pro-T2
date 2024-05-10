a36d706c0bcb1e751ed1f416056b0fc7df2a1c06
Schlenge <emil.tarala@gmail.com>
2024-05-10 20:33:01 +0300

    commit-emil-2

---
+++ src/components/RestrictedRoute.js
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+//import { selectIsLoggedIn } from 'redux/auth/authSelectors';
+import { selectIsLoggedIn } from '../redux/auth/authSelectors';
+
+const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
+
+export default RestrictedRoute;
